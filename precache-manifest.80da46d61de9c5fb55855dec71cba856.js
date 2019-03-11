a613ded3be6812ca79bc332af6f36f01ec6f2e19
ajtalford <ajtalford@gmail.com>
2019-03-11 18:27:04 -0400

    Updates

---
+++ precache-manifest.80da46d61de9c5fb55855dec71cba856.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "4b31330b20e3cff3e49d540e4e7175e2",
+    "url": "/github.io/DevUps/static/media/showcase.4b31330b.jpg"
+  },
+  {
+    "revision": "d5a70aef47e2f1cb02b2",
+    "url": "/github.io/DevUps/static/js/runtime~main.a2e49299.js"
+  },
+  {
+    "revision": "1e6cd708991ed13ca763",
+    "url": "/github.io/DevUps/static/js/main.39ef0191.chunk.js"
+  },
+  {
+    "revision": "8434f46e5abcf73ad6ee",
+    "url": "/github.io/DevUps/static/js/2.56e72f81.chunk.js"
+  },
+  {
+    "revision": "1e6cd708991ed13ca763",
+    "url": "/github.io/DevUps/static/css/main.bc094040.chunk.css"
+  },
+  {
+    "revision": "1b0d078974823605b9d4afcb9f91e632",
+    "url": "/github.io/DevUps/index.html"
+  }
+];
